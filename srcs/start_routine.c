1bfebad26979eecb74310ef1f0449aaa689adb58
Yohen Sheraun <yosherau@u80z02s03.42kl.edu.my>
2025-06-02 11:50:44 +0800

    Added eating functionality partially

--- srcs/start_routine.c
+++ srcs/start_routine.c
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 12:44:28 by yosherau          #+#    #+#             */
-/*   Updated: 2025/06/01 19:52:38 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/06/02 11:50:05 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,33 @@ static void	wait_threads(t_utils *utils)
 		;
 }
 
-// static void	start_eating(t_utils *utils)
-// {
-	
-// }
+static void	start_eating(t_utils *util, t_philo *philo)
+{
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(&util->forks[philo->left_fork].fork);
+		printf("Philo %d acquired left fork %d\n", philo->id, philo->left_fork);
+		pthread_mutex_lock(&util->forks[philo->right_fork].fork);
+		printf("Philo %d acquired right fork %d\n", philo->id, philo->right_fork);
+		pthread_mutex_unlock(&util->forks[philo->left_fork].fork);
+		pthread_mutex_unlock(&util->forks[philo->right_fork].fork);
+		printf("Philo %d has finished eating %d\n", philo->id);
+	}
+	else
+	{
+		pthread_mutex_lock(&util->forks[philo->right_fork].fork);
+		printf("Philo %d acquired right fork %d\n", philo->id, philo->right_fork);
+		pthread_mutex_lock(&util->forks[philo->left_fork].fork);
+ 		printf("Philo %d acquired left fork %d\n", philo->id, philo->left_fork);
+		pthread_mutex_unlock(&util->forks[philo->right_fork].fork);
+		pthread_mutex_unlock(&util->forks[philo->left_fork].fork);
+	}
+}
+
+void	start_sleeping(t_utils *util, t_philo *philo)
+{
+	usleep(util->time_to_sleep);
+}
 
 void	*start_routine(void *args)
 {
@@ -32,6 +55,6 @@ void	*start_routine(void *args)
 	philo = (t_philo *)args;
 	util = philo->util;
 	wait_threads(util);
-	// start_eating(&util);
+	start_eating(util, philo);
 	return (0);
 }
